ece4d6aa85f6c85bd69caac6a2b3937055cef8df
nazmul02902 <nazmulhasan02902@gmail.com>
2023-02-25 15:44:23 +0000

    python helo world

---
+++ helloWorld.py
@@ -0,0 +1,15 @@
+if 5 > 2:
+    print("Five is always greater than two");
+
+#This is a comment in python
+
+#variable
+x = 5
+y = 3
+
+print("The value of x and y is", x, y);
+
+#type checking
+converse_x = str(x)
+
+print(type(converse_x))
